cb86ce16b03952c3b526cef44fce2a31f207051f
greenprofessionals <green.professionals.network@gmail.com>
2026-08-07 21:36:14 -0400

    Update site-config.js

--- site-config.js
+++ site-config.js
@@ -1,4 +1,4 @@
 window.EVENT_TICKETING_CONFIG = {
   ENDPOINT: 'https://script.google.com/macros/s/AKfycbzOAdOtboSSQBBbVfGQ2SLX8GNb0hrH0zJIaK61ISE_qTWLi6RQQSSjLnXtxp--V_56/exec',
-  TICKETING_URL: 'https://greenprofessionals.github.io/event-ticketing-v6/'
+  TICKETING_URL: 'https://greenprofessionals.github.io/event-gate-v6/'
 };
